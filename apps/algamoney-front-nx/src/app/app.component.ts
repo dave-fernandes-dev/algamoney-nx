fcaa1b954673f85bcc67df1f81d08e16417c0661
Dave Fernandes Dev <dave.fernandes.dev@gmail.com>
2022-04-28 20:21:58 -0300

    initial commit

---
+++ apps/algamoney-front-nx/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'algamoney-nx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'algamoney-front-nx';
+}
